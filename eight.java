63d93f6701690325e2baa5107eec06d6b0b637ae
iamashishxo <ashishrajput1234567890@gmail.com>
2025-07-20 16:11:33 +0530

    20-07-2025

--- eight.java
+++ eight.java
@@ -2,6 +2,9 @@ public class eight {
     public static void main(String[] args) {
 
         int arr[] = { 2, 5, 7, 89, 43, 56, 78, 320, 500 };
+        int arr1[] = new int[5];
+        arr1[4] = 10;
+        System.out.println(arr1[3]);
 
         // // find largest number
         // int max = Integer.MIN_VALUE;
@@ -31,23 +34,23 @@ public class eight {
         // }
         // System.out.println("Third largest is " + thirdMax);
 
-        int max = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        int thirdMax = Integer.MIN_VALUE;
-        for (int num : arr) {
-            if (num > max) {
-                thirdMax = secondMax;
-                secondMax = max;
-                max = num;
-            } else if (num > secondMax && num != max) {
-                thirdMax = secondMax;
-                secondMax = num;
-            } else if (num > thirdMax && num != max && num != secondMax) {
-                thirdMax = num;
-            }
-        }
-        System.out.println("largest is " + max);
-        System.out.println("Second largest is " + secondMax);
-        System.out.println("Third largest is " + thirdMax);
+        // int max = Integer.MIN_VALUE;
+        // int secondMax = Integer.MIN_VALUE;
+        // int thirdMax = Integer.MIN_VALUE;
+        // for (int num : arr) {
+        // if (num > max) {
+        // thirdMax = secondMax;
+        // secondMax = max;
+        // max = num;
+        // } else if (num > secondMax && num != max) {
+        // thirdMax = secondMax;
+        // secondMax = num;
+        // } else if (num > thirdMax && num != max && num != secondMax) {
+        // thirdMax = num;
+        // }
+        // }
+        // System.out.println("largest is " + max);
+        // System.out.println("Second largest is " + secondMax);
+        // System.out.println("Third largest is " + thirdMax);
     }
 }
---
+++ nine.java
@@ -0,0 +1,24 @@
+public class nine {
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 2, 3, 5, 7, 11 };
+
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+        for (int a : arr) {
+            System.out.println(a);
+        }
+
+    }
+}
