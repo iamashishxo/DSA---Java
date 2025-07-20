fbd80fb68243e47ea02a33b939e9940f54ca3ff3
iamashishxo <ashishrajput1234567890@gmail.com>
2025-07-20 15:25:19 +0530

    20-07-2025

---
+++ eight.java
@@ -0,0 +1,53 @@
+public class eight {
+    public static void main(String[] args) {
+
+        int arr[] = { 2, 5, 7, 89, 43, 56, 78, 320, 500 };
+
+        // // find largest number
+        // int max = Integer.MIN_VALUE;
+
+        // for (int i = 0; i < arr.length; i++) {
+        // if (max < arr[i]) {
+        // max = arr[i];
+        // }
+        // }
+        // System.out.println("largest is " + max);
+
+        // int secondMax = Integer.MIN_VALUE;
+        // // second max
+        // for (int i = 0; i < arr.length; i++) {
+        // if (secondMax < arr[i] && arr[i] != max) {
+        // secondMax = arr[i];
+        // }
+        // }
+        // System.out.println("Second largest is " + secondMax);
+
+        // int thirdMax = Integer.MIN_VALUE;
+        // // second max
+        // for (int i = 0; i < arr.length; i++) {
+        // if (thirdMax < arr[i] && arr[i] != max && arr[i] != secondMax) {
+        // thirdMax = arr[i];
+        // }
+        // }
+        // System.out.println("Third largest is " + thirdMax);
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        int thirdMax = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                thirdMax = secondMax;
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num != max) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (num > thirdMax && num != max && num != secondMax) {
+                thirdMax = num;
+            }
+        }
+        System.out.println("largest is " + max);
+        System.out.println("Second largest is " + secondMax);
+        System.out.println("Third largest is " + thirdMax);
+    }
+}
